f493eaf5bbaeb2b7f7c30595859c81abf53147cc
JeerapatJOBBY <job.ljee555@gmail.com>
2020-11-22 19:08:04 +0700

    can make number 2 position

--- Project/CaesarCipher/CaesarCipher.ino
+++ Project/CaesarCipher/CaesarCipher.ino
@@ -1,4 +1,3 @@
-
 #include <Keypad.h>
 
 const byte ROWS = 4; //four rows
@@ -16,21 +15,32 @@ byte colPins[COLS] = {6, 7, 8, 9}; //connect to the column pinouts of the keypad
 //initialize an instance of class NewKeypad
 Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
 
-String alphabet[] = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
-int position = 0; 
+String alphabet[] = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};   //ประกาศอาเรย์ตัวหนังสืออยู่ตรงนี้
+
+
+
+int time = 1;
+String Position1 , Position2 ,Number ;
 
 void setup(){
   Serial.begin(9600);
 }
-  
+
+
+
 void loop(){
   char customKey = customKeypad.getKey();
   
-  String cipher[26];
-
-  if (customKey == number [position])
-  {
-      position ++;
+  if (customKey && time == 1){
+    Position1 = customKey;
+    Serial.println(Position1);
+    time = 2; //กำหนดตัวเลขลำดับที่ 1
   }
-  
+  else if (customKey && time == 2) {
+    Position2 = customKey;
+    Number = String(Position1) + String(Position2);
+    Serial.println( Number );
+    time = 1; //กำหนดตัวเลขลำดับที่ 2
+  }
+
 }
